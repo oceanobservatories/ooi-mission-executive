5f406b3bf10dfaaa57ad88fe704b2a52404683e3
Peter Cable <petercable@gmail.com>
2016-07-06 14:07:09 -0700

    Added setup.py
    Change-Id: If9593be6d9270a5b70cf42162abb9c2e5140853e

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='OOI Mission Executive',
+    version='0.0.1',
+    long_description=__doc__,
+    packages=['ooi_executive'],
+    include_package_data=True,
+    zip_safe=False,
+)
